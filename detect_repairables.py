ac0d52953ae5fed93e91b1cc1fc27b530d64810b
Joona Heikkilä <21111572+cxcorp@users.noreply.github.com>
2022-05-16 15:40:32 +0300

    flann+sift test

--- detect_repairables.py
+++ detect_repairables.py
@@ -58,35 +58,27 @@ def doThing():
     cv.namedWindow('image')
 
     tbs = ChangeTrackingTrackbars('image')
-    # tbs.add("maskThreshold").min(1).max(255).initial(200).build()
-    # tbs.add("blockSize").min(1).max(100).initial(2).build()
-    tbs.add("block").min(3).max(49).initial(3).build()
-    disti_max = 700
-    tbs.add("dist").min(0).max(disti_max).initial(10).build()
+    # tbs.add("block").min(3).max(49).initial(3).build()
+    # disti_max = 700
+    # tbs.add("dist").min(0).max(disti_max).initial(10).build()
 
-    cv.imshow('image', img)
+    #cv.imshow('image', img)
 
-    # blurred = cv.GaussianBlur(img, (3, 3), 0)
-    # lab = cv.cvtColor(blurred, cv.COLOR_BGR2Lab)
+    blurred = cv.GaussianBlur(img, (3, 3), 0)
+    lab = cv.cvtColor(blurred, cv.COLOR_BGR2Lab)
+    redSample = (123, 0, 0)
+    redSampleLab = rgb2lab(redSample)
+    redTreshold = 4
+    maskThreshold = 200
+    lower = np.clip([x - redTreshold for x in redSampleLab], 0, 255)
+    upper = np.clip([x + redTreshold for x in redSampleLab], 0, 255)
+    mask = cv.inRange(lab, lower, upper)
 
-    # redSample = (123, 0, 0)
-    # redSampleLab = rgb2lab(redSample)
-
-    # redTreshold = 4
-
-    # maskThreshold = 200 #tbs.getPos("maskThreshold")
-
-    # lower = np.clip([x - redTreshold for x in redSampleLab], 0, 255)
-    # upper = np.clip([x + redTreshold for x in redSampleLab], 0, 255)
-    # mask = cv.inRange(lab, lower, upper)
-
-    # mask = cv.dilate(mask, cv.getStructuringElement(
-    #     cv.MORPH_RECT, (maskThreshold, maskThreshold)))
-
+    mask = cv.dilate(mask, cv.getStructuringElement(
+        cv.MORPH_RECT, (maskThreshold, maskThreshold)))
     # img = cv.bitwise_and(img, img, mask=mask)
 
     #img = cv.medianBlur(img, ksize=3)
-    
 
     click = None
 
@@ -98,70 +90,59 @@ def doThing():
 
     cv.setMouseCallback('image', mouseListener)
 
+    sift = cv.SIFT_create()
 
-    img = cv.GaussianBlur(img, (5, 5), 0)
-    b, g, r = cv.split(img)
+    img1 = cv.split(img)[2]
+    img2 = cv.split(templateImages[0][0])[2]
 
-    imgWidth,imgHeight,_ = img.shape
+    kp1, des1 = sift.detectAndCompute(img1, mask)
+    kp2, des2 = sift.detectAndCompute(img2, None)
+
+    FLANN_INDEX_KDTREE = 1
+    index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+    search_params = dict(checks=50)
+    flann = cv.FlannBasedMatcher(index_params, search_params)
+
+    matches = flann.knnMatch(des1, des2, k=2)
+
+    matchesMask = [[0, 0] for i in range(len(matches))]
+    # ratio test as per Lowe's paper
+    for i, (m, n) in enumerate(matches):
+        if m.distance < 0.7*n.distance:
+            matchesMask[i] = [1, 0]
+
+    draw_params = dict(matchColor=(0, 255, 0),
+                       singlePointColor=(0, 0, 255),
+                       matchesMask=matchesMask,
+                       flags=cv.DrawMatchesFlags_DEFAULT)
 
     while True:
         if tbs.changed:
             # print('changed')
             tbs.changed = False
 
+            result = cv.drawMatchesKnn(
+                img1, kp1, img2, kp2, matches, None, **draw_params)
+
             # offset = tbs.getPos("offset")
             # lower = np.clip([x - offset for x in graySample], 0, 255)
             # upper = np.clip([x + offset for x in graySample], 0, 255)
             # grayMask = cv.inRange(lab, lower, upper)
             #result = img.copy()
-            block = tbs.getPos("block")
-            if block % 2 == 0:
-                block = block + 1
-
-            #img = [r,g,b][rgb]
-            #img = (255-img.copy())
-
-            # 3 or 9
-
-            # a1 = cv.adaptiveThreshold(
-            #     (255-g), 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY_INV, block, 2)
-            # a2 = cv.adaptiveThreshold(
-            #     (255-b), 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY_INV, block, 2)
-            # img = cv.bitwise_or(a1, a2)
-            img = g.copy()
-            img = cv.Canny(img, 20, 80)
-
-            contours, hierarchy = cv.findContours(
-                img, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
-            img = cv.cvtColor(img, cv.COLOR_GRAY2BGR)
-
-            distRel = tbs.getPos("dist") / float(disti_max)
-            dist = imgHeight * distRel
-
 
             #ret = cv.matchShapes(cnt1, cnt2, 1, 0.0)
-            cv.putText(img, f'dist%: {distRel:.3f}', (438, 395),
-                       cv.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2)
-            cv.putText(img, f'dist: {dist:.3f}', (438, 395+40),
-                       cv.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2)
-
-
-            textJobs = []
-            for cnt in contours:
-                x, y, w, h = cv.boundingRect(cnt)
-                if w > dist and h > dist:
-                    cv.rectangle(img, (x, y), (x+w, y+h), (0, 0, 255), 2)
-                    textJobs.append([img, f'{w/float(h):.2f}', (x,y), cv.FONT_HERSHEY_PLAIN, 1, (0,0,0), 5])
-                    textJobs.append([img, f'{w/float(h):.2f}', (x,y), cv.FONT_HERSHEY_PLAIN, 1, (0,0,255)])
-            
-            for job in textJobs:
-                cv.putText(*job)
+            # cv.putText(img, f'dist%: {distRel:.3f}', (438, 395),
+            #            cv.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2)
+            # cv.putText(img, f'dist: {dist:.3f}', (438, 395+40),
+            #            cv.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2)
 
             if click is not None:
-                cv.putText(img, f'({click[0]}, {click[1]})', (15, 90),
+                cv.putText(result, f'({click[0]}, {click[1]})', (15, 90),
                            cv.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2)
 
-            cv.imshow('image', img)
+            #cv.imshow('image', result)
+            plt.imshow(result,)
+            plt.show()
         k = cv.waitKey(1) & 0xFF
         if k == EXIT_KEY:
             break
