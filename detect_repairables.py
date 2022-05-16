2df9a97e3ed6109a2a32277eba43307a0d3d1799
Joona Heikkilä <21111572+cxcorp@users.noreply.github.com>
2022-05-16 15:02:42 +0300

    contours shower

--- detect_repairables.py
+++ detect_repairables.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
+from operator import index
 import sys
 import cv2 as cv
+from cv2 import blur
 import numpy as np
 import math
 import imutils
+from matplotlib import pyplot as plt
 from trackbars import ChangeTrackingTrackbars
 
 IMG_A = "./a.png"
@@ -25,13 +28,27 @@ def rgb2lab(rgb):
     lab = cv.cvtColor(img, cv.COLOR_RGB2Lab)
     return lab[0][0]
 
+
 templates = [
-    "exploded_panel"
+    "burnt_out_compressor",
+    "containn",
+    "corroded_tanks",
+    "damaged_gears",
+    "damaged_hydraylics",
+    "exploded_panel",
+    "hull_fracture",
+    "melted_fuel_cell",
+    "radiation_leak",
+    "rusted",
+    "scorched_plating",
+    "shattered"
 ]
+
 
 def doThing():
     img = cv.imread(IMG_A)
-    templateImages = [(cv.imread(f'./images/{template}.png'), template) for template in templates]
+    templateImages = [
+        (cv.imread(f'./images/{template}.png'), template) for template in templates]
 
     if img is None:
         sys.exit("Couldn't read image")
@@ -41,58 +58,110 @@ def doThing():
     cv.namedWindow('image')
 
     tbs = ChangeTrackingTrackbars('image')
-    maxThreshold = 700
     # tbs.add("maskThreshold").min(1).max(255).initial(200).build()
     # tbs.add("blockSize").min(1).max(100).initial(2).build()
-    tbs.add("threshold").min(0).max(maxThreshold).initial(maxThreshold).build()
+    tbs.add("block").min(3).max(49).initial(3).build()
+    disti_max = 700
+    tbs.add("dist").min(0).max(disti_max).initial(10).build()
 
     cv.imshow('image', img)
 
-    blurred = cv.GaussianBlur(img, (3, 3), 0)
-    lab = cv.cvtColor(blurred, cv.COLOR_BGR2Lab)
+    # blurred = cv.GaussianBlur(img, (3, 3), 0)
+    # lab = cv.cvtColor(blurred, cv.COLOR_BGR2Lab)
 
-    redSample = (123, 0, 0)
-    redSampleLab = rgb2lab(redSample)
+    # redSample = (123, 0, 0)
+    # redSampleLab = rgb2lab(redSample)
 
-    redTreshold = 4
+    # redTreshold = 4
 
-    maskThreshold = 200 #tbs.getPos("maskThreshold")
+    # maskThreshold = 200 #tbs.getPos("maskThreshold")
 
-    lower = np.clip([x - redTreshold for x in redSampleLab], 0, 255)
-    upper = np.clip([x + redTreshold for x in redSampleLab], 0, 255)
-    mask = cv.inRange(lab, lower, upper)
+    # lower = np.clip([x - redTreshold for x in redSampleLab], 0, 255)
+    # upper = np.clip([x + redTreshold for x in redSampleLab], 0, 255)
+    # mask = cv.inRange(lab, lower, upper)
 
-    mask = cv.dilate(mask, cv.getStructuringElement(
-        cv.MORPH_RECT, (maskThreshold, maskThreshold)))
+    # mask = cv.dilate(mask, cv.getStructuringElement(
+    #     cv.MORPH_RECT, (maskThreshold, maskThreshold)))
+
+    # img = cv.bitwise_and(img, img, mask=mask)
+
+    #img = cv.medianBlur(img, ksize=3)
     
-    img = cv.bitwise_and(img, img, mask=mask)
+
+    click = None
+
+    def mouseListener(event, x, y, flags, param):
+        nonlocal click, tbs
+        if event == cv.EVENT_LBUTTONDOWN:
+            click = (x, y)
+            tbs.changed = True
+
+    cv.setMouseCallback('image', mouseListener)
+
+
+    img = cv.GaussianBlur(img, (5, 5), 0)
+    b, g, r = cv.split(img)
+
+    imgWidth,imgHeight,_ = img.shape
 
     while True:
         if tbs.changed:
             # print('changed')
             tbs.changed = False
 
+            # offset = tbs.getPos("offset")
+            # lower = np.clip([x - offset for x in graySample], 0, 255)
+            # upper = np.clip([x + offset for x in graySample], 0, 255)
+            # grayMask = cv.inRange(lab, lower, upper)
             #result = img.copy()
-            threshold = 0.9 #tbs.getPos("threshold") / float(maxThreshold)
-            result = img.copy()
+            block = tbs.getPos("block")
+            if block % 2 == 0:
+                block = block + 1
 
-            for template, templateName in templateImages:
-                w, h, _ = template.shape
-                res = cv.matchTemplate(img, template, cv.TM_CCOEFF_NORMED)
+            #img = [r,g,b][rgb]
+            #img = (255-img.copy())
 
-                loc = np.where(res >= threshold)
-                for pt in zip(*loc[::-1]):
-                    cv.rectangle(result, pt, (pt[0]+w, pt[1]+h), (0, 0, 255), 2)
-                    cv.putText(result, templateName, (pt[0], pt[1]), cv.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 5)
-                    cv.putText(result, templateName, (pt[0], pt[1]), cv.FONT_HERSHEY_PLAIN, 1, (0, 0, 255), 1)
+            # 3 or 9
 
+            # a1 = cv.adaptiveThreshold(
+            #     (255-g), 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY_INV, block, 2)
+            # a2 = cv.adaptiveThreshold(
+            #     (255-b), 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY_INV, block, 2)
+            # img = cv.bitwise_or(a1, a2)
+            img = g.copy()
+            img = cv.Canny(img, 20, 80)
+
+            contours, hierarchy = cv.findContours(
+                img, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
+            img = cv.cvtColor(img, cv.COLOR_GRAY2BGR)
+
+            distRel = tbs.getPos("dist") / float(disti_max)
+            dist = imgHeight * distRel
+
+
+            #ret = cv.matchShapes(cnt1, cnt2, 1, 0.0)
+            cv.putText(img, f'dist%: {distRel:.3f}', (438, 395),
+                       cv.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2)
+            cv.putText(img, f'dist: {dist:.3f}', (438, 395+40),
+                       cv.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2)
+
+
+            textJobs = []
+            for cnt in contours:
+                x, y, w, h = cv.boundingRect(cnt)
+                if w > dist and h > dist:
+                    cv.rectangle(img, (x, y), (x+w, y+h), (0, 0, 255), 2)
+                    textJobs.append([img, f'{w/float(h):.2f}', (x,y), cv.FONT_HERSHEY_PLAIN, 1, (0,0,0), 5])
+                    textJobs.append([img, f'{w/float(h):.2f}', (x,y), cv.FONT_HERSHEY_PLAIN, 1, (0,0,255)])
             
-            #result = cv.bitwise_and(result, result, mask=mask)
+            for job in textJobs:
+                cv.putText(*job)
 
-            cv.putText(result, f'threshold: {threshold:.3f}', (15, 30), cv.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2)
+            if click is not None:
+                cv.putText(img, f'({click[0]}, {click[1]})', (15, 90),
+                           cv.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2)
 
-            cv.imshow('image', result)
-
+            cv.imshow('image', img)
         k = cv.waitKey(1) & 0xFF
         if k == EXIT_KEY:
             break
